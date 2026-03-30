12d03e524a1e308469cc3ee2d58826b2d261c416
Mahima <mahimarawat0707@gmail.com>
2026-03-30 20:31:02 +0530

    updated

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/gigshield")
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
+
+app.use("/", routes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
